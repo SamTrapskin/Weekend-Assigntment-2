41463fbac6f90bc6c695e9d217cdc5b9e85dfbd8
Sam Trapskin <samtrapskin@gmail.com>
2018-03-17 14:33:46 -0500

    404

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,41 +2,85 @@ $(document.ready).ready(readyNow);
 
 function readyNow () {
     console.log('JS is ready');
-     $('#plus').on ('click ')
+     $('#plus').on ('click ', addNums);
+
 }
 
-        function Addnums () {
-            num1 + num2 
-        }
+        function addNums () {
+            let x = $('#numberOne').val();
+            let y = $('#numberTwo').val();
+            let numbersToAdd = {
+                    x: x,
+                    y: y,
+                    type: "Add"
+                 };
 
-        function numsMinus () {
-            num1 - num2
-        }
-
-        function numsMult () {
-            num1 * num2
-        }
-
-        function numsDivide () {
-            num1 / num2
-        }
-        function getAddAnswer() {
+                 x + y;
+                
+             
             $.ajax({
-              type: 'GET',
-              url: '/addition',
-            }).done(function(response) {
-                console.log("SUCCESS!!!");
-                // append products to the dom
-                addProductsToDOM(response);
-            }).fail(function(response) {
-                // notify the user
-                alert('request failed');
-              }
-            );
-          }
-          function addNumbersToDOM(products) {
-            for(let i = 0; i < products.length; i += 1) {
-              // append to the dom
+                type: 'POST',
+                data: numbersToAdd,
+                url:'/number'
+            }).done(function(response){
+                    console.log('SUCCESS!');
+            }).fail(function(response){
+                alert('something went wrong...');
+            })
+           console.log(parseInt(x + y));
+        }
+        function showCalcHistory () {
+        $.ajax({
+            type: "GET",
+            url: '/number'
+          }).done(function(response){
+            appendToDom(response);
+          })
+        }
+
+        function appendToDom(showCalcHistory){
+            $('#answerHistory').empty();
+            
+              let tr = $("<tr></tr>");
+              tr.append('<td>' + num1+num2 + '</td>');
+             
+              $('#answerHistory').append(tr);
+              
             }
-          }
+          
+
+
+
+
+
+        // function numsMinus () {
+        //     num1 - num2
+        // }
+
+        // function numsMult () {
+        //     num1 * num2
+        // }
+
+        // function numsDivide () {
+        //     num1 / num2
+        // }
+        // function getAddAnswer() {
+        //     $.ajax({
+        //       type: 'GET',
+        //       url: '/addition',
+        //     }).done(function(response) {
+        //         console.log("SUCCESS!!!");
+        //         // append products to the dom
+        //         addProductsToDOM(response);
+        //     }).fail(function(response) {
+        //         // notify the user
+        //         alert('request failed');
+        //       }
+        //     );
+        //   }
+        //   function addNumbersToDOM(products) {
+        //     for(let i = 0; i < products.length; i += 1) {
+        //       // append to the dom
+        //     }
+        //   }
           
--- server/server.js
+++ server/server.js
@@ -1,8 +1,15 @@
 let express = require('express');
 let app = express();
-let PORT = 5555;
-
+const PORT = 5555;
+app.use(bodyPareser.urlencoded({exteded:true}))
 app.use(express.static('server/public'));
+
+app.post('/number', (req,res)=> {
+  let numbersToAdd = req.body;
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
 
 app.listen(PORT, function(){
     console.log('listening on port', PORT);
